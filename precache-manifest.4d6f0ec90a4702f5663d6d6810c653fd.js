afa46370b298af0d509fd63381a4689928b9d5c7
ptanzy <ptanzy@comcast.net>
2019-04-06 16:25:25 -0400

    Updates

---
+++ precache-manifest.4d6f0ec90a4702f5663d6d6810c653fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  },
+  {
+    "revision": "696c82b07547839d2bbe",
+    "url": "/Clicky-Game/static/js/main.b20c35bd.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "696c82b07547839d2bbe",
+    "url": "/Clicky-Game/static/css/main.086b7c13.chunk.css"
+  },
+  {
+    "revision": "53a20217decac62c820b3d5fb5ba2e4c",
+    "url": "/Clicky-Game/index.html"
+  }
+];
